f01e6ee40e09602a7a520678f2d4ddd797c01391
Alfian Maulana Malik <alfian.malik@gmail.com>
2022-01-03 14:38:49 +0700

    update

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -55,13 +55,34 @@
                             <td>{{ $order->lawyer_schedule }}</td>
                             <th>
                                 <button class="btn btn-outline-{{ $order->is_waiting?"primary":($order->is_finished?"danger":"primary") }} btn-sm" {{ $order->is_waiting?"disabled":($order->is_finished?"Live Chat":"disabled") }}>
-                                    <i class="fa fa-circle" style="font-size: 6px;top: -10px" aria-hidden="true"></i> {{ $order->is_waiting?"Proses":($order->is_finished?"Live Chat":"Pending") }}
+                                    <i class="fa fa-circle" style="font-size: 6px;top: -10px" aria-hidden="true"></i> {{ $order->is_waiting?"Proses":($order->is_finished?"Live Chat":"Live Chat") }}
                                 </a>
                             </th>
                             <th>
-                                <button class="btn btn-outline-primary btn-sm">
+                                <button class="btn btn-outline-primary btn-sm" data-toggle="modal" data-target="#view{{ $order->id }}">
                                     <i class="fa fa-eye"></i>
                                 </button>
+                                
+                                <!-- Modal -->
+                                <div class="modal fade" id="view{{ $order->id }}" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Body
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <a href="{{ route("chat.show", $order->order_uuid) }}" class="btn btn-outline-primary btn-sm {{ $order->is_waiting?"Proses":($order->is_finished?"Live Chat":"disabled") }}">
                                     <i class="fa fa-comments"></i> Live Chat
                                 </a>
--- resources/views/layout/backend/sidebar.blade.php
+++ resources/views/layout/backend/sidebar.blade.php
@@ -42,20 +42,21 @@
     </li>
 
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#product"
+            aria-expanded="true" aria-controls="product">
             <i class="fas fa-users"></i>
             <span>Produk</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="product" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{ route('user') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Customer</span>
+                    <span>Layanan Mitra</span>
                 </a>
                 <a class="collapse-item" href="{{ route('lawyers') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span></span>
+                    <span>Layanan Dokumen</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Artikel & E Journal</span>
                 </a>
             </div>
         </div>
@@ -93,20 +94,42 @@
     </li>
 
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#setting"
+            aria-expanded="true" aria-controls="setting">
             <i class="fas fa-users"></i>
             <span>Setting</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="setting" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{ route('user') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Customer</span>
+                    <span>Notifikasi</span>
                 </a>
                 <a class="collapse-item" href="{{ route('lawyers') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span></span>
+                    <span>Tentang Kami</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>FAQ</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Hubungi Kami</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Syarat & Ketentuan</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Syarat & Ketentuan Mitra</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Kebijakan Privasi</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Gabung HaloLawyer</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Ingin Bekerja Sama?</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Kontributar HaloLawyer</span>
                 </a>
             </div>
         </div>
@@ -121,12 +144,13 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{ route('user') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>Customer</span>
+                    <span>Semua User</span>
                 </a>
                 <a class="collapse-item" href="{{ route('lawyers') }}">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span></span>
+                    <span>Profile Saya</span>
+                </a>
+                <a class="collapse-item" href="{{ route('lawyers') }}">
+                    <span>Add New Admin</span>
                 </a>
             </div>
         </div>
