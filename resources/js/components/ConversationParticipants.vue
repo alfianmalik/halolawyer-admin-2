c3817e5b07945452c46779a94b13e513a18456f6
Alfian Maulana Malik <alfian.malik@gmail.com>
2022-01-06 13:39:18 +0700

    update

--- resources/js/components/ConversationParticipants.vue
+++ resources/js/components/ConversationParticipants.vue
@@ -1,43 +1,42 @@
 <template>
   <div class="card card-default">
     <div class="card-body">
-      <div>
+      <div class="d-none">
           <h6 class="font-weight-bold">Mitra Hukum</h6>
       </div>
-      <div class="media">
+      <div class="media d-none">
           <img class="mr-3 rounded-circle" src="https://via.placeholder.com/50" alt="Generic placeholder image">
           <div class="media-body mt-2">
             <h6 class="mt-0 font-weight-bold mt-1">Rifai Ghufron, S.H</h6>
           </div>
       </div>
-      <div>
+      <div class="d-none">
           <div class="mt-2 mb-2">Bidang Keahlian dan Spesialisasi</div>
           <div>
               Hutang Piutang, Keluarga,
               Ketenagakerjaan, Pidana dan Laporan
               Polisi, Pertanahan dan Properti, Bisnis,
               dan Teknologi Informasi
-
           </div>
       </div>
-      <div class="mt-5">
+      <div class="mt-1">
           <h6 class="font-weight-bold">Customer</h6>
       </div>
-      <div class="media">
-          <img class="mr-3 rounded-circle" src="https://via.placeholder.com/50" alt="Generic placeholder image">
-          <div class="media-body">
-            <h6 class="mb-0 font-weight-bold">Customer Name</h6>
-            <div>yuda@gmail.com</div>
-            <div>09128866</div>
-          </div>
+      <div v-for="(participant, index) in participants" :key="index">
+        <div class="media" v-if="participant.participation[1].messageable_type == 'App\\Models\\Administrator'">
+            <img class="mr-3 rounded-circle" src="https://via.placeholder.com/50" alt="">
+            <div class="media-body">
+              <h6 class="mb-0 font-weight-bold"></h6>
+              <div>{{ participant.first_name }} {{ participant.last_name }}</div>
+              <div>{{ participant.phone }}</div>
+            </div>
+        </div>
       </div>
-
-
       <ul class="chat d-none">
         <li class="left clearfix border-bottom py-1" v-for="(participant, index) in participants" :key="index">
           <div class="chat-body clearfix">
             <div class="d-flex align-items-center header">
-              <div class="primary-font participant-name">{{ participant.name }}</div>
+              <div class="primary-font participant-name">{{ participant.first_name }}</div>
               <span class="participant-type pl-1"> ({{ participant.participation[0].messageable_type}})</span>
             </div>
           </div>
